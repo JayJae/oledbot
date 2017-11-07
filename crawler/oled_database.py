17ea36038a3a7e039b795f70db4bea7414698d94
jayjae <wigglegigglegoggle@gmail.com>
2017-11-07 21:51:53 +0900

    Modify database

--- crawler/oled_database.py
+++ crawler/oled_database.py
@@ -1,6 +1,5 @@
 from database import Database
 import pymysql
-import unidecode
 import json
 
 class OledDatabase(Database) :
@@ -39,6 +38,22 @@ class OledDatabase(Database) :
                                 eval_result['content'], eval_result['agency'],
                                 eval_result['purchasers'], eval_result['open_time'],
                                 eval_result['ending_date']))
+                except UnicodeEncodeError :
+                    try :
+                        eval_result['name'] = eval_result['name'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
+                        eval_result['no'] = eval_result['no'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '').replace('Bidding NO', '').strip()
+                        eval_result['content'] = eval_result['content'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
+                        eval_result['agency'] = eval_result['agency'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
+                        eval_result['purchasers'] = eval_result['purchasers'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
+                        eval_result['open_time'] = eval_result['open_time'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
+                        eval_result['ending_date'] = eval_result['ending_date'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
+
+                        cur.execute(sql, (eval_result['name'], eval_result['no'], eval_result['content'],
+                                    eval_result['agency'], eval_result['purchasers'],
+                                    eval_result['open_time'], eval_result['ending_date']))
+                    except pymysql.err.IntegrityError :
+                        continue
+
                 except pymysql.err.IntegrityError :
                     continue
 
@@ -46,7 +61,7 @@ class OledDatabase(Database) :
 
 
     def select_eval_results(self, purchaser, date)  :
-        sql = '''select * from EVAL_RESULTS where PURCHASERS="%s" and OPEN_TIME>="%s"''' % (purchaser, date)
+        sql = '''select * from EVAL_RESULTS where PURCHASERS="%s" and INSERT_DATE>="%s"''' % (purchaser, date)
         eval_results = []
         print(sql)
         with self.conn.cursor() as cur :
@@ -153,13 +168,7 @@ class OledDatabase(Database) :
                         award['winner'] = award['winner'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
                         award['manufacturer'] = award['manufacturer'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
                         award['country'] = award['country'].replace('\uff1f', '').replace('\uff1a', '').replace('\u3000', '')
-                        """
-                        award['name'] = unidecode.unidecode(award['name'])
-                        award['content'] = unidecode.unidecode(award['content'])
-                        award['purchasers'] = unidecode.unidecode(award['purchasers'])
-                        award['winner'] = unidecode.unidecode(award['winner'])
-                        award['manufacturer'] = unidecode.unidecode(award['manufacturer'])
-                        """
+
                         cur.execute(sql, (award['name'], award['no'], award['content'],
                                     award['agency'], award['purchasers'],
                                     award['open_time'], award['eval_result'],
@@ -173,8 +182,11 @@ class OledDatabase(Database) :
         self.conn.commit()
 
     def select_tender_awards(self, purchaser, date) :
-        sql = '''select * from TENDER_AWARDS
-                where PURCHASERS="%s" and BIDDING_RESULT_DATE>="%s"''' % (purchaser, date)
+        sql = '''select PROJECT_NAME, BIDDING_NO,
+                BIDDING_CONTENT, BIDDING_AGENCY, PURCHASERS, OPEN_TIME,
+                EVAL_RESULT_DATE, BIDDING_RESULT_DATE, FINAL_WINNER,
+                MANUFACTURER, COUNTRY from TENDER_AWARDS
+                where PURCHASERS="%s" and INSERT_DATE>="%s"''' % (purchaser, date)
         tender_awards = []
 
         with self.conn.cursor() as cur :
@@ -196,3 +208,32 @@ class OledDatabase(Database) :
 
                 tender_awards.append(award)
         return tender_awards
+
+    def select_all_tender_awards(self) :
+        sql = '''select PROJECT_NAME, BIDDING_NO,
+                BIDDING_CONTENT, BIDDING_AGENCY, PURCHASERS, OPEN_TIME,
+                EVAL_RESULT_DATE, BIDDING_RESULT_DATE, FINAL_WINNER,
+                MANUFACTURER, COUNTRY from TENDER_AWARDS'''
+        tender_awards = []
+
+        with self.conn.cursor() as cur :
+            cur.execute(sql)
+            rows = cur.fetchall()
+            for row in rows :
+                award = {}
+                award['name'] = row[0]
+                award['no'] = row[1]
+                award['content'] = row[2]
+                award['agency'] = row[3]
+                award['purchasers'] = row[4]
+                award['open_time'] = row[5]
+                award['eval_result'] = row[6]
+                award['bidding_result'] = row[7]
+                award['winner'] = row[8]
+                award['manufacturer'] = row[9]
+                award['country'] = row[10]
+
+                tender_awards.append(award)
+        return tender_awards
+
+
